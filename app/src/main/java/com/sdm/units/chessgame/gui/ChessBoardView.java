22e4196e9c75612152cb5c5970fbebc8a765bca0
dellimone <dellimone99@gmail.com>
2025-02-17 19:05:32 +0100

    add create board with correct number of squares, click on square call controller

--- app/src/main/java/com/sdm/units/chessgame/gui/ChessBoardView.java
+++ app/src/main/java/com/sdm/units/chessgame/gui/ChessBoardView.java
@@ -4,16 +4,47 @@ import com.sdm.units.chessgame.gamelogic.ChessboardFile;
 import com.sdm.units.chessgame.gamelogic.ChessboardPosition;
 import com.sdm.units.chessgame.gamelogic.ChessboardRank;
 
-public class ChessBoardView {
-    public void highlightSquare(ChessboardPosition chessboardPosition) {
-        ;
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessBoardView extends JPanel {
+    private final ChessController controller;
+    private final JButton[][] squares;
+
+    public ChessBoardView(ChessController controller) {
+        this.controller = controller;
+        this.squares = new JButton[8][8];
+        setLayout(new GridLayout(8, 8));
+        initializeBoard();
     }
 
+    private void initializeBoard() {
+        for (int rank = 7; rank >= 0; rank--) {  // Start from 7 to match chess ranks (1-8)
+            for (int file = 0; file < 8; file++) {
+                JButton square = new JButton();
+                squares[rank][file] = square;
+                final ChessboardPosition position = new ChessboardPosition(
+                        ChessboardFile.values()[file],
+                        ChessboardRank.values()[rank]
+                );
+                square.addActionListener(e -> controller.handleSquareClick(position));
+                add(square);
+            }
+        }
+    }
+
+    
+    public void highlightSquare(ChessboardPosition position) {
+        // To be implemented
+    }
+
+    
     public void clearHighlights() {
-        ;
+        // To be implemented
     }
 
-    public void updateBoard() {
-
+    
+    public void updateBoard(ChessBoardViewModel viewModel) {
+        // To be implemented
     }
 }
--- app/src/test/java/test/chessgame/gui/ChessBoardViewTest.java
+++ app/src/test/java/test/chessgame/gui/ChessBoardViewTest.java
@@ -1,4 +1,48 @@
-package test.chessgame.gui;
+package com.sdm.units.chessgame.gui;
 
-public class ChessBoardViewTest {
+import com.sdm.units.chessgame.gamelogic.ChessboardFile;
+import com.sdm.units.chessgame.gamelogic.ChessboardPosition;
+import com.sdm.units.chessgame.gamelogic.ChessboardRank;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import javax.swing.JButton;
+import java.awt.Component;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class ChessBoardViewTest {
+    private ChessBoardView view;
+    private ChessController mockController;
+
+    @BeforeEach
+    void setUp() {
+        mockController = mock(ChessController.class);
+        view = new ChessBoardView(mockController);
+    }
+
+    @Test
+    void shouldCreateBoardWithCorrectNumberOfSquares() {
+        // A chess board should have 64 squares (8x8)
+        Component[] components = view.getComponents();
+        long buttonCount = countButtons(components);
+        assertEquals(64, buttonCount);
+    }
+
+    private long countButtons(Component[] components) {
+        return java.util.Arrays.stream(components)
+                .filter(component -> component instanceof JButton)
+                .count();
+    }
+
+    @Test
+    void shouldCallControllerWhenSquareIsClicked() {
+        // Get the button at position A1 (0,7 in the grid)
+        JButton buttonA1 = (JButton) view.getComponents()[56]; // 7 * 8 + 0 = 56
+        buttonA1.doClick();
+
+        // Verify that the controller was called with position A1
+        verify(mockController).handleSquareClick(
+                new ChessboardPosition(ChessboardFile.A, ChessboardRank.ONE)
+        );
+    }
 }
